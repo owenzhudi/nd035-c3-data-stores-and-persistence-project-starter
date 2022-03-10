befa638403ac56288768e9d10382bad04181d86b
Di Zhu <dizhu@Dis-MacBook-Pro.local>
2022-03-09 16:48:43 -0800

    add config and entities

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
@@ -0,0 +1,75 @@
+package com.udacity.jdnd.course3.critter.pet;
+
+import com.udacity.jdnd.course3.critter.user.Customer;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@Entity
+public class Pet {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private PetType type;
+
+    @Nationalized
+    private String name;
+
+    @ManyToOne
+    private Customer owner;
+
+    private LocalDate birthDate;
+    private String notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Employee.java
@@ -0,0 +1,53 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.DayOfWeek;
+import java.util.Set;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Nationalized
+    private String name;
+    private Set<EmployeeSkill> skills;
+    private Set<DayOfWeek> daysAvailable;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<EmployeeSkill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<EmployeeSkill> skills) {
+        this.skills = skills;
+    }
+
+    public Set<DayOfWeek> getDaysAvailable() {
+        return daysAvailable;
+    }
+
+    public void setDaysAvailable(Set<DayOfWeek> daysAvailable) {
+        this.daysAvailable = daysAvailable;
+    }
+}
